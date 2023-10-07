349131995ce3e8e5f90e9b6b94a032447f8e3032
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-10-07 10:45:12 -0600

    Actualizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express'); //Traemos a express
 //Cada instancia de express sera una aplicación
+const morgan = require ('morgan');
+
 const app = express(); //Instanciamos y creamos la app del servidor
 
-app.listen(3000,() => {  //escuchador
-    console.log("Server listening on port 3000");
-}); 
+//Settings consifuraciones del servidor/Aplicación
+app.set('PORT',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+//Middlewares
+
+
+app.use(express.json());
+
+
